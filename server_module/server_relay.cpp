a407a7a67badf1b9e34742945a855750d0d6be76
H2 <h.mwondi@gmail.com>
2024-05-26 14:25:40 -0400

    Added server module package to enable streaming onto web hosted display

---
+++ server_module/server_relay.cpp
@@ -0,0 +1,19 @@
+// Created to run on linux environment by importing the libraries particular to linux environment
+// The ".h" files
+
+#include <iostream>
+#include "string.h"
+
+
+// ??Windows configuration 
+// Using the libraries :
+// Implement later when windows configuration is needed
+
+// #include <winsock2.h>
+// #include "Ws2_32.lib"
+
+
+
+int main(){
+    return 0;
+}
